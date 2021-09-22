187eab362164102940d137ec3bfea8c3f5f33add
salman alfariji <79994933+salmanti20@users.noreply.github.com>
2021-09-22 16:58:41 +0700

    Add files via upload

---
+++ Java11.java
@@ -0,0 +1,15 @@
+
+package java1.pkg1;
+
+/**
+ *
+ * @author salman alfariji
+ */
+public class Java11 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
---
+++ Java12.java
@@ -0,0 +1,13 @@
+
+package java1.pkg1;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author salman alfariji
+ */
+public class Java12 {
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
